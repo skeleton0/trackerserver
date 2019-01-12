99d218e04736d7d32fa29614f5476a179b010fbc
skeleton0 <34125010+skeleton0@users.noreply.github.com>
2019-01-12 20:17:24 +1100

    Update main class to instantiate Database

--- TrackerServer.java
+++ TrackerServer.java
@@ -1,5 +1,11 @@
 class TrackerServer {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        try {
+            Database db = new Database("tracker_server.db", false);
+        } catch (Exception e) {
+            System.err.print("Caught exception: ");
+            System.err.println(e.getMessage());
+        }
     }
 }
