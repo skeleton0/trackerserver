4cd2f4d3b49cbb8b4954ce2309ea9be7da6b2e50
skeleton0 <34125010+skeleton0@users.noreply.github.com>
2019-01-28 21:43:01 +1100

    Add saving of http post data and a method for getting it

--- src/main/java/trackerserver/Network.java
+++ src/main/java/trackerserver/Network.java
@@ -1,10 +1,15 @@
 package trackerserver;
 
 import fi.iki.elonen.NanoHTTPD;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.logging.Logger;
 
 class Network extends NanoHTTPD {
     private static final Logger LOG = Logger.getLogger(Network.class.getName());
+    private final ArrayBlockingQueue<String> mTrackerUpdates = new ArrayBlockingQueue<String>(10);
 
     Network(int port) {
         super(port);
@@ -14,6 +19,37 @@ class Network extends NanoHTTPD {
     public Response serve(IHTTPSession session) {
         LOG.info("Received connection from " + session.getRemoteHostName() + " (" + session.getRemoteIpAddress() + ")");
 
+        Map<String, String> files = new HashMap<String, String>();
+
+        try {
+            session.parseBody(files);
+        } catch (Exception e) {
+            System.err.println("parseBody threw an Exception.");
+            System.err.println(e.getMessage());
+        }
+
+        String value = files.get("postData");
+        if (value != null) {
+            while (true) {
+                try {
+                    mTrackerUpdates.put(value);
+                    break;
+                } catch (InterruptedException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }
+
         return super.serve(session);
+    }
+
+    String takeNextUpdate() {
+        while (true) {
+            try {
+                return mTrackerUpdates.take();
+            } catch (InterruptedException e) {
+                System.err.println(e.getMessage());
+            }
+        }
     }
 }
