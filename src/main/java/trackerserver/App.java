fabde6a01dcf8870b6a71ff8d5176faec636731c
skeleton0 <34125010+skeleton0@users.noreply.github.com>
2019-01-30 23:22:23 +1100

    Update main to receive tracker updates and insert them into database

--- src/main/java/trackerserver/App.java
+++ src/main/java/trackerserver/App.java
@@ -1,18 +1,35 @@
 package trackerserver;
 
+import java.util.logging.Logger;
+
 class App {
+    private static final Logger LOG = Logger.getLogger(App.class.getSimpleName());
+
     public static void main(String[] args) {
+        Database database = null;
+        Network network = null;
+
         try {
-            Database db = new Database("tracker_server.db", false);
+            database = new Database("tracker_server.db", false);
 
-            Network net = new Network(46000);
-            net.start();
-
-            Thread.sleep(60000);
-            net.stop();
+            network = new Network(46000);
+            network.start();
         } catch (Exception e) {
-            System.err.print("Caught exception: ");
+            System.err.print("Caught exception during initialisation of database and network: ");
             System.err.println(e.getMessage());
-        }  
+        }
+
+        while (true) {
+            String update = network.takeNextUpdate();
+
+            LOG.info("Received update: " + update);
+            LOG.info("Parsing update to LocationUpdate...");
+
+            try {
+                database.insertLocationUpdate(LocationUpdate.fromString(update));
+            } catch (Exception e) {
+                LOG.warning(e.getMessage());
+            }
+        }
     }
 }
