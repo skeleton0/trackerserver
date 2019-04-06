a76b25cbadab0f9757c148a2a7d390bf700bb87b
skeleton0 <34125010+skeleton0@users.noreply.github.com>
2019-04-06 18:00:09 +1100

    Instantiate database and tracker http server in main

--- src/main/java/trackerserver/App.java
+++ src/main/java/trackerserver/App.java
@@ -7,11 +7,22 @@ class App {
     private static final Logger LOG = Logger.getLogger(App.class.getSimpleName());
 
     public static void main(String[] args) {
-        LOG.info("Starting up...");
-
         CountDownLatch shutdownLatch = new CountDownLatch(1);
-
         Runtime.getRuntime().addShutdownHook(new Thread(shutdownLatch::countDown));
+
+        Database db = null;
+        TrackerHttpServer trackerServer = null;
+
+        try {
+            db = new Database("tracker_server.db", false);
+
+            trackerServer = new TrackerHttpServer(46000, db);
+            trackerServer.makeSecure(trackerServer.makeSSLSocketFactory("/keystore.jks", "4bZteOGV7P0LEIBfnsN5".toCharArray()), null);
+            trackerServer.start();
+        } catch (Exception e) {
+            LOG.info("Caught exception during initialisation: " + e.getMessage());
+            shutdownLatch.countDown();
+        }
 
         try {
             shutdownLatch.await();
@@ -20,6 +31,9 @@ class App {
             LOG.info("Received the following excuse for interruption: " + e.getMessage());
         }
 
-        LOG.info("Shutting down...");
+        LOG.info("Waiting for existing connections to finish...");
+        if (trackerServer != null) trackerServer.stop();
+
+        LOG.info("Shutting down.");
     }
 }
